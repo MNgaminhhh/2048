951fd42471c188ee58a7ac290dbee07215a2feb7
MN <57828955+MNgaminhhh@users.noreply.github.com>
2025-01-22 09:17:11 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import BoardView from "./components/Board";
+import Mdk from "@interlinklabs/mdk";
+import "./main.scss";
+import "./styles.scss";
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLoginFailure = () => {
+    console.error("Login failed");
+  };
+
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <Mdk
+          appid="id__hl0k5j8r1rp5mz35ggb0"
+          onSuccess={handleLoginSuccess}
+          onFailure={handleLoginFailure}
+        >
+          {({ open }) => (
+            <div className="centerContainer">
+              <h2>2048</h2>
+              <button
+                className="resetButton"
+                onClick={open}
+                style={{ padding: "10px 20px", fontSize: "16px" }}
+              >
+                Sign In
+              </button>
+            </div>
+          )}
+        </Mdk>
+      ) : (
+        <>
+          <h1>2048</h1>
+          <BoardView />
+        </>
+      )}
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
